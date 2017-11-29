68c57aaffe84d22182c5fdd65b2903418ba11d80
Alex Williams <alexwilliams567@gmail.com>
2017-11-29 15:00:53 +0000

    find project example

---
+++ find-project.js
@@ -0,0 +1,19 @@
+const url = 'mongodb://localhost:27017/learnyoumongo';
+const mongo = require('mongodb').MongoClient;
+
+let age = parseInt(process.argv[2]);
+let resultsArr = [];
+mongo.connect(url, (err, db) => {
+   if (err) throw err;
+   let parrots = db.collection('parrots');
+   parrots.find({},{name: 1, age: 1, _id: 0}).toArray((err, result) => {
+       if (err) throw err;
+       result.filter(document => {
+          if (document.age > age) {
+              resultsArr.push(document);
+          }
+       });
+       console.log(resultsArr);
+   });
+   db.close();
+});
